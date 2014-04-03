de7f65de35cd3889fe0cc21a5812b51fdff29f46
henns20 <jjhennelly@gmail.com>
2014-04-02 23:26:38 -0400

    Create insertionSort.js

---
+++ example-code/insertionSort.js
@@ -0,0 +1,2 @@
+// from codecademy  http://www.codecademy.com/groups/advanced-javascript-coders/discussions/5308a69a282ae30d5f0008c8
+// the following solution was from the following web page http://www.nczonline.net/blog/2012/09/17/computer-science-in-javascript-insertion-sort/
